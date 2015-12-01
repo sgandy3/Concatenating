2385a5732ac2ce5668497f75cd808ed2cde83aad
Your Name <you@example.com>
2015-12-01 07:46:16 -0500

    C# Concatenating Strings

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Ivan";
+            string lastName = "Ivanov";
+             Console.WriteLine("Hello, {0}!\n", firstName);
+
+             string fullname = firstName + " " + lastName;
+             Console.WriteLine("Your full name is {0}.", fullname);
+
+            Console.WriteLine("Hello" +" " + firstName +" "+ lastName);
+
+
+        }
+    }
+}
